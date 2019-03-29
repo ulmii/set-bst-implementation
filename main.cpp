a95c4d65d62ed25eb9fe4e256aa1f79c93e10b2f
Konrad Ulman <veradopl@gmail.com>
2019-03-29 21:42:19 +0100

    Changed test in main

--- main.cpp
+++ main.cpp
@@ -7,28 +7,44 @@ int main()
 
 	MySet<int> set;
 
-	set.insert(1);
-	set.insert(0);
-	set.insert(2);
-	set.insert(3);
-	set.insert(5);
-	set.insert(9);
-	set.insert(34);  
+	set.insert(8);
+	set.insert(4);
+	set.insert(10);  
+	set.insert(10);
+	set.insert(2);  
+	set.insert(5); 
+	set.insert(9);    
+	set.insert(12);
+	set.insert(12);      
+	set.insert(1);  
+	set.insert(14);   
 	set.insert(7);
-
-    cout << "size: " << set.size() << "\n";
+    
+    set.remove(4);    
+    set.remove(12);  
+    set.remove(14);  
 
     for(auto it = set.begin(); it != set.end(); ++it)
-    {
+    {  
         cout << *it << ", ";
-    }
-
-    cout << std::endl;
-
-    for(auto it : set)
-    {
-        cout << it << ", ";
-    }
+    }  
+      
+    cout << std::endl;  
  
+    auto set2 = set;
+    cout  << "size: " << set.size() << " set: " << set;
+    cout  << "size: " << set2.size() << " set2: " <<  set2;
+
+    for(const auto& it : set) 
+    {     
+        cout << it << ", ";  
+    }
     cout << std::endl;
-}
+   
+    for(auto it = set2.begin(); it != set2.end(); ++it)
+    {   
+        cout << *it << ", "; 
+    }
+
+    cout << "\n";
+} 
